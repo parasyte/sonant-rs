70c809ab80b779fcdb317a153d17222921c6f950
Jay Oster <jay@kodewerx.org>
2019-10-28 21:04:51 -0700

    Minimal documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! A Rust port of the [Sonant 4K synth](http://www.pouet.net/prod.php?which=53615) with streaming
+//! support.
+//!
+//! Sonant [(C) 2008-2009 Jake Taylor](https://creativecommons.org/licenses/by-nc-sa/2.5/)
+//! [ Ferris / Youth Uprising ]
+//!
+//! # Crate features
+//!
+//! - `std` (default) - Allow `std::error::Error`. Disable default features to use `sonant` in a
+//!   `no_std` environment.
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 mod consts;
